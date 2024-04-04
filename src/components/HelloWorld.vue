ef6201c01c40fc1aea27574fa34bc25c787744fc
Naldo Reupo <naldo_reupo-musayon@mckinsey.com>
2024-04-03 20:03:05 -0500

    chore : initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,57 @@
+<script setup>
+import SupportIcon from "@/components/icons/IconSupport.vue";
+import WelcomeItem from "@/components/WelcomeItem.vue";
+
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      You’ve successfully created a project with {{ msg }}
+      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+    </h3>
+  </div>
+    <WelcomeItem>
+        <template #icon>
+            <SupportIcon />
+        </template>
+        <template #heading>Support Vue</template>
+
+        As an independent project, Vue relies on community backing for its sustainability. You can help
+        us by
+        <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.
+    </WelcomeItem>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
