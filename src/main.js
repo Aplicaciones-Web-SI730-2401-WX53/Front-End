95ef3d72c41621bfb289a7a9d919a615611a9462
Naldo Reupo <naldo_reupo-musayon@mckinsey.com>
2024-04-08 19:54:41 -0500

    chore: add primevue

--- src/main.js
+++ src/main.js
@@ -2,5 +2,15 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from "primevue/config";
+import 'primevue/resources/themes/aura-light-green/theme.css'
+import Button from 'primevue/button';
+import InputText from 'primevue/inputtext';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(PrimeVue);
+app.component('pv-button',Button)
+app.component('pv-input-text',InputText)
+
+app.mount('#app')
